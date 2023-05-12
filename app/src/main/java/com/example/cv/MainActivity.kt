d38c4461fa741ae3b1ff65d82c85befc5b88e851
TomasSt365 <lapink365@gmail.com>
2023-05-12 16:23:06 +0200

    Create FirstInfo design

---
+++ app/src/main/java/com/example/cv/MainActivity.kt
@@ -0,0 +1,87 @@
+package com.example.cv
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.example.cv.ui.theme.CVTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CVTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    MainScreen()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun MainScreen(modifier: Modifier = Modifier) {
+}
+
+@Composable
+fun FirstInfo(
+    painterImage: Painter,
+    name: String,
+    title: String,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterImage,
+            contentDescription = null
+        )
+
+        Text(
+            text = name,
+            fontSize = 32.sp,
+            modifier = modifier,
+        )
+
+        Text(
+            text = title,
+            fontFamily = FontFamily.Cursive,
+            fontSize = 22.sp
+        )
+    }
+}
+
+@Composable
+fun ContactInfo() {
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    CVTheme {
+        MainScreen()
+        FirstInfo(
+            painterImage = painterResource(id = R.drawable.author_foto),
+            name = "Nikolas Lapin",
+            title = "Junior Android Developer"
+        )
+    }
+}
