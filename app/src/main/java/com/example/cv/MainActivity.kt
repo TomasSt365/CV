702cb8ef492aa75ddad1083462f361dc7c6be9a4
TomasSt365 <lapink365@gmail.com>
2023-05-12 21:58:53 +0200

    version 1.0

--- app/src/main/java/com/example/cv/MainActivity.kt
+++ app/src/main/java/com/example/cv/MainActivity.kt
@@ -1,23 +1,36 @@
 package com.example.cv
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.Center
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.cv.ui.theme.CVTheme
-import com.example.cv.ui.theme.compose.*
+import com.example.cv.ui.theme.compose.Email
+import com.example.cv.ui.theme.compose.Link
+import com.example.cv.ui.theme.compose.PhoneLink
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,13 +50,36 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MainScreen(modifier: Modifier = Modifier) {
-    ContactInfo(
-        mobilePhone = "+48733696872",
-        email = "lapink365@gmail.com",
-        linkedIn = "https://www.linkedin.com/in/nikolas-lapin-35a877275/",
-        gitHub = "https://github.com/TomasSt365"
-    )
+fun MainScreen() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color(0x5900BCD4))
+    ) {
+
+        FirstInfo(
+            painterImage = painterResource(R.drawable.author_foto),
+            name = stringResource(R.string.author_name),
+            title = stringResource(R.string.author_title),
+            modifier = Modifier
+                .align(Center)
+                .padding(bottom = 70.dp)
+        )
+
+        ContactInfo(
+            fontSize = 24.sp,
+            mobilePhone = stringResource(R.string.phone_number),
+            email = stringResource(R.string.email),
+            linkedIn = stringResource(R.string.linkedIn),
+            gitHub = stringResource(R.string.git_hub),
+            modifier = Modifier
+                .padding(bottom = 70.dp)
+                .align(Alignment.BottomCenter)
+        )
+
+
+    }
+
 }
 
 @Composable
@@ -53,63 +89,80 @@ fun FirstInfo(
     title: String,
     modifier: Modifier = Modifier
 ) {
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Image(
-            painter = painterImage,
-            contentDescription = null
-        )
+    Box(modifier = modifier) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painter = painterImage,
+                contentDescription = null
+            )
 
-        Text(
-            text = name,
-            fontSize = 32.sp,
-            modifier = modifier,
-        )
+            Text(
+                text = name,
+                fontWeight = FontWeight.Bold,
+                fontSize = 40.sp,
+            )
 
-        Text(
-            text = title,
-            fontFamily = FontFamily.Cursive,
-            fontSize = 22.sp
-        )
+            Text(
+                text = title,
+                color = Color(0xFF0A490D),
+                fontWeight = FontWeight.Bold,
+                fontFamily = FontFamily.Cursive,
+                fontSize = 32.sp
+            )
+        }
     }
 }
 
+@SuppressLint("ModifierParameter")
 @Composable
 fun ContactInfo(
     mobilePhone: String,
+    fontSize: TextUnit,
     email: String,
     linkedIn: String? = null,
     gitHub: String? = null,
+    modifier: Modifier = Modifier
 ) {
-    Column() {
+    Box(modifier = modifier) {
+        Column {
 
-        PhoneLink(phoneNumber = mobilePhone)
-
-        Email(email = email)
-
-        if (linkedIn != null) {
-            Link(
-                uri = linkedIn,
-                shortName = "LinkedIn"
+            PhoneLink(
+                phoneNumber = mobilePhone,
+                fontSize = fontSize
             )
-        }
 
-        if (gitHub != null) {
-            Link(
-                uri = gitHub,
-                shortName = "GitHub"
+            Email(
+                email = email,
+                fontSize = fontSize
             )
-        }
 
+            if (linkedIn != null) {
+                Link(
+                    uri = linkedIn,
+                    shortName = "LinkedIn",
+                    fontSize = fontSize
+                )
+            }
+
+            if (gitHub != null) {
+                Link(
+                    uri = gitHub,
+                    shortName = "GitHub",
+                    fontSize = fontSize
+                )
+            }
+
+        }
     }
-}
 
+}
 
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun CVPreview() {
     CVTheme {
         MainScreen()
     }
